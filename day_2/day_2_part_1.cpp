4554b0b214c05a2154f607412c75d107c69add86
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-12-02 23:14:57 +1300

    added testing file

---
+++ day_2/day_2_part_1.cpp
@@ -0,0 +1,12 @@
+// be wery the input can be various length
+
+/*
+Thesis
+    - go threw each line 
+    - have a flag for all increasing, all decreasing and 1 < abs(diff) < 3
+    - flag when equal numbers are seen (could be done by 
+      keeping track fo previous index)
+    - running sum for unsafe or safe by looking at the flags 
+      (a certain combo of flags is bad and another combo is good)
+*/
+
