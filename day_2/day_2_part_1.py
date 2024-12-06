b604f9947728cc875a8357fe8c6440506f19b13e
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-12-06 20:45:27 +1300

    added some code#

---
+++ day_2/day_2_part_1.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    
+    increasing_flag = False
+    decreasing_flag = False
+    
+    num_safe = 0
+    
+    with open('testing2.txt', 'r') as file:
+        for line in file:
+            line_of_nums = line.split(" ")
+            curr_num = line_of_nums[0]
+            for i in range(len(1,line_of_nums)):
+                pass
+
+                
