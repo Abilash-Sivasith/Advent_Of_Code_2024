03d217645d8d0980fb1032cdf600b29a99a28ff6
Abilash Sivasith <abi.sivasith.world@gmail.com>
2024-12-07 12:07:07 +1300

    Day 2, Part 2 - UnSolved

--- day_2/day_2_part_2.py
+++ day_2/day_2_part_2.py
@@ -1,3 +1,5 @@
+
+
 def main():
     """main logic"""
     num_safe_lines = 0
@@ -8,59 +10,71 @@ def main():
             line_as_list_of_nums = line.split(" ")
             decreasing_flag = is_decreasing(line_as_list_of_nums)
             increasing_flag = is_increasing(line_as_list_of_nums)
-            duplicates_flag, duplicated_number = contains_duplicate(line_as_list_of_nums)
-            contains_multiple_duplicates_flag = contains_multiple_duplicates(line_as_list_of_nums)
-            # print(f"line {line_as_list_of_nums}, is XOR = {XOR(decreasing_flag, increasing_flag)} and {adjacent_num_diff(line_as_list_of_nums)}")
+            duplicate_flag, duplicated_num = contains_duplicate(line_as_list_of_nums)
+            
+            if XOR(decreasing_flag, increasing_flag) and adjacent_num_diff(line_as_list_of_nums) and not duplicate_flag:
+                print(f"line --> {line} is SAFE")
+                num_safe_lines += 1
 
-            if XOR(decreasing_flag, increasing_flag) and not duplicates_flag:
-                if (adjacent_num_diff(line_as_list_of_nums)):
+            elif not duplicate_flag:
+                for i in range(len(line_as_list_of_nums)):
+                    copy = line_as_list_of_nums[:]
+                    copy.pop(i)
+                    increasing_flag = is_increasing(copy)
+                    decreasing_flag = is_decreasing(copy)
+                    if XOR(increasing_flag, decreasing_flag) and adjacent_num_diff(copy):
+                        print(f"line --> {line} is SAFE")
+                        num_safe_lines += 1
+                        break
+            
+
+            elif duplicate_flag and not contains_multiple_duplicates(line_as_list_of_nums):
+                #   print(f"line --> {line} is in")
+                copy_of_list = line_as_list_of_nums[:]
+                copy_of_list.remove(duplicated_num)
+                increasing_flag = is_increasing(copy_of_list)
+                decreasing_flag = is_decreasing(copy_of_list)
+                if XOR(decreasing_flag, increasing_flag) and adjacent_num_diff(copy_of_list):
                     num_safe_lines += 1
-                else:
-                    for i in range(0, len(line_as_list_of_nums)):
-                        copy_of_list = line_as_list_of_nums[:]
-                        copy_of_list.pop(i)
-                        if adjacent_num_diff(copy_of_list):
-                            num_safe_lines += 1
-                            break
-            elif duplicates_flag and not contains_multiple_duplicates_flag:
-                copy = line_as_list_of_nums[:]
-                copy.remove(duplicated_number)
-                decreasing_flag = is_decreasing(copy)
-                increasing_flag = is_increasing(copy)
-                if (adjacent_num_diff(copy)) and XOR(decreasing_flag, increasing_flag):
-                    num_safe_lines += 1
-                    
+                    print(f"line --> {line} is SAFE")
 
-
- 
+                
+            
     print(f"The number of safe lines --> {num_safe_lines}")
 
-def contains_multiple_duplicates(list_of_nums):
-    """returns true is there is multiple duplicates"""
-    duplicate_count = 0
-    seen_keys = dict()
+def contains_duplicate(list_of_nums):
+    """retruns true if it contains duplicates and returns the first duplicate number"""
+    seen_dict = dict()
     for num in list_of_nums:
-        if num not in seen_keys:
-            seen_keys[num] = 1
+        if num not in seen_dict:
+            seen_dict[num] = 1
         else:
-            duplicate_count += 1
+            return (True, num)
+    
+    return (False, -1)
 
-        if duplicate_count == 2:
+def contains_multiple_duplicates(list_of_nums):
+    """returns true if the list contains multiple duplicates"""
+    seen_dict = dict()
+    for num in list_of_nums:
+        if num not in seen_dict:
+            seen_dict[num] = 1
+        else:
+            seen_dict[num] += 1
+    
+    dups_seen = 0 
+    for key, value in seen_dict.items():
+        if value == 2:
+            dups_seen += 1
+        elif value >= 3:
+            return True
+        
+        if dups_seen >= 2:
             return True
     
     return False
-
-
-def contains_duplicate(list_of_nums):
-    """returns true if the lift contains duplicates"""
-    seen_keys = dict()
-    for num in list_of_nums:
-        if num not in seen_keys:
-            seen_keys[num] = 1
-        else:
-            return True, num
     
-    return False, -1
+
 
 def adjacent_num_diff(list_of_nums):
     """returns true is adjacent numbers differ by atleast 1 and max of 3"""
@@ -71,7 +85,7 @@ def adjacent_num_diff(list_of_nums):
     return True
 
 def XOR(a, b):
-    """does XOR logic between a and b"""
+    """repliactes XOR logic"""
     if a != b:
         return True
     else:
